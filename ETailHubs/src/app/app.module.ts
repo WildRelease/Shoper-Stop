fe1f0d7fe1029380749db9dd216b789f202a59dd
aman <aman@podiotracker.com>
2024-01-09 15:33:28 +0530

    angular material installed

--- ETailHubs/src/app/app.module.ts
+++ ETailHubs/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MaterialModule } from 'src/material_modules';
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +17,8 @@ import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
