1bdc93b2bc037c07156a77f236e66d3085eab46c
tejasdabare12 <tejasdabare12@gmail.com>
2024-01-10 19:33:36 +0530

    FormsUpdate

--- ETailHubs/src/app/sign-up/sign-up.component.ts
+++ ETailHubs/src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
+  
+  signupForm!: FormGroup;
+  fb: FormBuilder = new FormBuilder;
+
+  ngOnInit(): void {
+    this.signupForm= new FormGroup({
+      fullName:new FormControl("",Validators.required),
+      phoneNo: new FormControl("",Validators.maxLength(10)),
+      email: new FormControl ('', [Validators.required, Validators.email]),
+    })
+  }
+
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      
+      console.log('Form submitted:', this.signupForm.value);
+    }
+  }
+  
 }
